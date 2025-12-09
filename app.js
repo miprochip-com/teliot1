e3f5a29cf48e0cac9659b397f6f8aece220cfb40
miprochip-com <miprochip.com@gmail.com>
2025-12-09 17:28:51 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -378,7 +378,7 @@ function downloadCsv() {
   const subset = clean.slice(-50);
 
   const headerText =
-    "Informe Teliot1 en ubicación :MA-AX_4567. Informe y web desarrollado por https://www.miprochip.com";
+    "Informe Teliot1 en ubicacion :MA-AX_4567. Informe y web desarrollado por https://www.miprochip.com";
 
   const now = new Date();
   const fechaInforme = now.toLocaleString("es-ES", {
